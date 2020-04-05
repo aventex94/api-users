02e123d3bbadf1904d04a0598da0dae7d24e8db0
aventex94 <matiasmiletich.informatica@gmail.com>
2020-04-05 17:47:43 -0300

    fix

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
 // This will be our application entry. We'll setup our server here.
 const http = require('http');
 const app = require('./core/app'); // The express app we just created
-
 const models = require('./models');
-
 
 const server = http.createServer(app);
 
